33c2bdec6440e1930f966da4fe6be61040c6c587
Dotan Nahum <jondotan@gmail.com>
2019-12-29 21:00:59 +0200

    kingpin

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
 
 // or kingpin for nicer CLI
 
@@ -9,11 +12,11 @@ var VERSION = "dev"
 var _ = VERSION
 
 var ip = flag.Int("flagname", 1234, "help message for flagname")
-
-// var debug   = kingpin.Flag("debug", "Enable debug mode.").Bool()
+var debug = kingpin.Flag("debug", "Enable debug mode.").Bool()
 
 func main() {
 	flag.Parse()
+	app := kingpin.New("completion", "My application with bash completion.")
 
 	f := NewFarble(&Counter{})
 
